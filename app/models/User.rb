91e66f449e11ee3bd36331586cdadf20dedca795
Brad Reid <brad.reid@canadadrugs.com>
2011-08-28 23:04:52 -0500

    fix user model

--- app/models/User.rb
+++ app/models/User.rb
@@ -4,9 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-
-  has_many :projects
-
   attr_accessor :login
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :login, :username
