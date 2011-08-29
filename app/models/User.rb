48df2a50ec22645cbd940286a18230a688ebce58
Brad Reid <brad.reid@canadadrugs.com>
2011-08-28 23:16:46 -0500

    update devise

--- app/models/User.rb
+++ app/models/User.rb
@@ -4,17 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessor :login
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :login, :username
-
-
-  protected
-
-  def self.find_for_database_authentication(warden_conditions)
-    conditions = warden_conditions.dup
-    login = conditions.delete(:login)
-    where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-  end
-  
+  attr_accessible :email, :password, :password_confirmation, :remember_me
 end
