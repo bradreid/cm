9b3c8d23b90eea50e7d1d4c8a60b311de1d9083f
Brad Reid <brad.reid@canadadrugs.com>
2011-08-28 20:46:18 -0500

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cm::Application.config.secret_token = '5af883d8a6a4d3933602035224d9a6c05e98523beef6095e743c46ea68060b8b594bafc700fb333709b3e3a40727e689ca4c038d58c46ba0e8324c22112a2c21'
