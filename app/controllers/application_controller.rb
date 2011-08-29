5cf81923cae23b76b822c0d60bc55d7df1c88932
Brad Reid <brad.reid@canadadrugs.com>
2011-08-29 09:28:13 -0500

    add basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :verify_access
+  
+  protected
+  
+  # Use basic authentication in my realm to get a user object.
+  # Since this is a security filter - return false if the user is not
+  # authenticated.
+  def verify_access
+    authenticate_or_request_with_http_basic("Documents Realm") do |username, password|
+      username == 'rdi' && password == 'btc'
+    end
+  end  
 end
