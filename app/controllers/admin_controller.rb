f381dfac013cdfcbccb1b49de9b70b3122b63d4c
Brad Reid <brad.reid@canadadrugs.com>
2011-09-21 20:30:52 -0500

    add basic nav

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,10 @@
 class AdminController < ApplicationController
-  before_filter :authenticate_user!  
+  before_filter :authenticate_user!, :set_area
+  
+private
+
+  def set_area
+    @area = :admin
+  end
   
 end
