7673e99fb6c92d458920501ca3763051c9d54c33
Brad Reid <brad.reid@canadadrugs.com>
2011-09-25 22:00:27 -0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -58,6 +58,9 @@ Cm::Application.routes.draw do
   resources :tools
   resources :guided_questions
   match 'about' => 'welcome#about'
+  match 'search' => 'tools#index'
+  match 'guided' => 'guided_questions#new'
+  
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
