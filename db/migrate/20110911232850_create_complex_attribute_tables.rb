d8635099da5de851d64975c218526475035c2614
Brad Reid <brad.reid@canadadrugs.com>
2011-10-03 21:48:21 -0500

    fix migrations

--- db/migrate/20110911232850_create_complex_attribute_tables.rb
+++ db/migrate/20110911232850_create_complex_attribute_tables.rb
@@ -1,6 +1,16 @@
 class CreateComplexAttributeTables < ActiveRecord::Migration
   def self.up
     create_table :tools, :force => true do |t|
+      t.string :name
+      t.string :method_of_access
+      t.float :cost_to_use
+      t.float :cost_to_obtain
+      t.integer :time_to_use
+      t.string :other_resources_needed
+      t.string :electronics_equipment
+      t.string :software
+      t.string :activities_involved
+      t.string :expertise_needed
       t.timestamps
     end
     add_column :tools, :reading_level, :string
@@ -9,9 +19,10 @@ class CreateComplexAttributeTables < ActiveRecord::Migration
   end
 
   def self.down
-    drop_table :tools
+   
     remove_column :tools, :steps
     remove_column :tools, :jargon
     remove_column :tools, :reading_level
+    drop_table :tools    
   end
 end
