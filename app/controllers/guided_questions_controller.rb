af42f9dee6a08f7bf627078d3e8a35b040adec66
Brad Reid <brad.reid@canadadrugs.com>
2011-10-23 22:04:29 -0500

    update phases

--- app/controllers/guided_questions_controller.rb
+++ app/controllers/guided_questions_controller.rb
@@ -1,5 +1,17 @@
 class GuidedQuestionsController < ApplicationController
   before_filter :set_area
+  
+  def index
+    @tools = Tool.paginate(:conditions => {:why => params[:purpose]},:page => params[:page], :per_page => 25)
+    respond_to do |wants|
+      wants.js do
+        render :update do |page|
+          page.call "$('#search_results').html", render(:partial => 'search_results')
+          page.call '$.Spinner.unspin'
+        end
+      end
+    end    
+  end
 
 
 private  
