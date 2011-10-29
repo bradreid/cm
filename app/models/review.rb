bedc348bd8ecfbb3398ecf4ceb4f1364ab32e654
Brad Reid <brad.reid@canadadrugs.com>
2011-10-28 21:25:43 -0500

    add validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,4 @@
 class Review < ActiveRecord::Base
-  validates_presence_of :name, :organization, :current_role, :tool_used, :goal_clear
+  validates_presence_of :name, :organization, :current_role, :tool_used, :goal_clear, :tasks_easy_to_understand, :additional_requirements_clear, :tasks_easy_to_implement
   belongs_to :tool
 end
