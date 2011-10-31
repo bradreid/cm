1bd0920dae8289cf4781337212be43c5b82a10fa
Brad Reid <brad.reid@canadadrugs.com>
2011-10-30 19:07:30 -0500

    update tic form order

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,20 +1,9 @@
 class Tool < ActiveRecord::Base
   has_attached_file :source_document, :storage => :s3, :s3_credentials => "#{RAILS_ROOT}/config/s3.yml", :path => "/:style/:filename"
   
-  validates_presence_of :name, :author, :language, :when, :why, :topic, :description, :copyright, :source_url
+  validates_presence_of :name, :source_document_name, :source_url, :author, :language, :description, :when, :why, :topic, :copyright
   validates_uniqueness_of :name
   belongs_to :created_by, :class_name => 'User', :foreign_key => 'created_by'
   has_many :reviews
-  
-  def where_question
-    JSON.parse(self.where) if self.where
-  end
-  
-  def where_question=(args)
-    self.where=args.to_json
-  end
-  
-  def select_true_attributes(attrs)
-    attrs.select{|attr| self.send(attr)}.map{|m| m.to_s.gsub(/^[^_]*_/, '')}
-  end 
+
 end
