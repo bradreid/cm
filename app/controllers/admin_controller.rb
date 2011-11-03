51c0784a55e01eb32c2436d6594cad37dc8d5065
Brad Reid <brad.reid@canadadrugs.com>
2011-11-02 21:27:56 -0500

    add admin users

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,3 +1,12 @@
 class AdminController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :authenticate_admin
+  
+  
+  def authenticate_admin
+     if !current_user.try(:is_admin?)
+       flash[:error] = "You have to be an admin"
+       redirect_to root_path
+       return false
+     end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111101011138) do
+ActiveRecord::Schema.define(:version => 20111103014835) do
 
   create_table "reviews", :force => true do |t|
     t.integer  "tool_id"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(:version => 20111101011138) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "username"
+    t.boolean  "is_admin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
