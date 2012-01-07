2d1032b52cface037ade1e23c7035663805c9121
Brad Reid <brad.reid@canadadrugs.com>
2012-01-06 21:57:43 -0600

    new reivew format

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,22 +1,23 @@
 class Review < ActiveRecord::Base
-  validates_presence_of :name, :organization, :current_role, :tool_used, :goal_clear, :tasks_easy_to_understand, :additional_requirements_clear, :tasks_easy_to_implement, :tasks_achieve_goal, :additional_resources_required_for_goal, :easily_adapted, :easily_adapted_cultural, :easily_merged, :insightful_resource, :perfect_to_use_as_presented
+  
+  validates_presence_of :tool_id, :experience, :community_context, :generates_results, :resources_identified, :format_presentation, :adaptable
   belongs_to :tool
-  
+  belongs_to :user
   before_save :add_rating
-  
-  def star_rating
-    ranking = 0.0
-    questions = [:goal_clear, :tasks_easy_to_understand, :additional_requirements_clear, :tasks_easy_to_implement, :tasks_achieve_goal, :additional_resources_required_for_goal, :easily_adapted, :easily_adapted_cultural, :easily_merged ]
-    questions.each do |q|
-      ranking += self.send(q).to_f
-    end
-    ranking = ranking / questions.size.to_f
-  end
-  
+  after_save :update_tool_stars
+   
 private
 
   def add_rating
-    self.rating = self.star_rating 
-    self.tool.add_rating(self.rating)
+    r = 0
+    questions = [:community_context, :generates_results, :resources_identified, :format_presentation, :adaptable]
+    questions.each do |q|
+      r += self.send(q)
+    end
+    self.rating = (r/2.0).ceil
+  end
+  
+  def update_tool_stars
+    self.tool.recalc_stars    
   end
 end
