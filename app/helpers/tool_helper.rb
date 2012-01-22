7f485970d5986d4cccc8bd87d1aa3fbf69a1a8c5
Brad Reid <brad.reid@canadadrugs.com>
2012-01-21 21:00:54 -0600

    ie fixes and view changes

--- app/helpers/tool_helper.rb
+++ app/helpers/tool_helper.rb
@@ -2,6 +2,10 @@ module ToolHelper
 
   
   def rating_stars(tool)
-    render :partial => 'shared/stars', :object => tool
+    render :partial => 'shared/tool_stars', :object => tool
+  end
+  
+  def review_stars(review)
+    render :partial => 'shared/review_stars', :object => review    
   end
 end
