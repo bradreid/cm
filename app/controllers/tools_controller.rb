b585b577b0b5e9c2b133157b7c4aa7dfa3d73442
Brad Reid <brad.reid@canadadrugs.com>
2012-03-24 13:02:06 -0500

    better advanced search

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -2,35 +2,34 @@ class ToolsController < ApplicationController
   before_filter :set_area
   
   def index
-    if params[:search]
-      search_tools   
+      
+      unless params[:search].blank?
+        search_tools
+      end 
       
       unless params[:language].blank?
-        @tools = @tools.where(:language => params[:language])
+        @tools = arel_start.where(:language => params[:language])
       end
       
       unless params[:author].blank?
-        tools=Tool.arel_table
-        @tools = @tools.where(tools[:author].matches("%#{params[:author]}%"))
+        @tools = arel_start.where(tools[:author].matches("%#{params[:author]}%"))
       end
       
       unless params[:time_to_use_as_seconds].blank?
-        @tools = @tools.where("time_to_use_as_seconds <= ?", params[:time_to_use_as_seconds].to_i.send(params[:time_to_use_type].downcase).seconds.to_i)
+        @tools = arel_start.where("time_to_use_as_seconds <= ?", params[:time_to_use_as_seconds].to_i.send(params[:time_to_use_type].downcase).seconds.to_i)
       end
       
       unless params[:rating].blank?
-        @tools = @tools.where("rating >= ?", params[:rating].to_i)
+        @tools = arel_start.where("rating >= ?", params[:rating].to_i)
       end
       
       unless params[:from].blank?
-        @tools = @tools.where("date_created >= ?", Date.parse("1/1/#{params[:from]}"))
+        @tools = arel_start.where("date_created >= ?", Date.parse("1/1/#{params[:from]}"))
       end
       
       unless params[:to].blank?
-         @tools = @tools.where("date_created <= ?", Date.parse("12/31/#{params[:to]}"))
-       end      
-      
-    end
+         @tools = arel_start.where("date_created <= ?", Date.parse("12/31/#{params[:to]}"))
+      end      
   end
   
   def show
@@ -38,17 +37,21 @@ class ToolsController < ApplicationController
   end
 
 private
+  
+  def arel_start
+    @tools ||= Tool.paginate default_pagination_params.merge(:order => 'name asc')
+  end
+
   def search_tools
     where_clause = []
     variables = []
-    @tools = Tool.paginate default_pagination_params.merge(:order => 'name asc')
     params[:search].split.each do |t|
       where_clause << "((UPPER(name) like UPPER(?)) OR (UPPER(description) like UPPER(?)))"
       variables << "%#{t}%"
       variables << "%#{t}%"      
     end
     sql = variables.inject([where_clause.join(' AND ')]){|sum, item|  sum << item}
-    @tools = @tools.where(sql)
+    @tools = arel_start.where(sql)
   end
   
   def set_area
