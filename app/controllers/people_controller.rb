6fcecc067905f90f810725fe7b69ffcf3a54a8cb
Brad Reid <brad.reid@canadadrugs.com>
2012-03-25 08:29:33 -0500

    full text for peoples controller

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -7,15 +7,24 @@ class PeopleController < ApplicationController
     end  
 
   private
-    def search_tools
-      where_clause = []
-      variables = []
-      params[:search].split.each do |t|
-        where_clause << "((UPPER(name) like UPPER(?)) OR (UPPER(description) like UPPER(?)))"
-        variables << "%#{t}%"
-        variables << "%#{t}%"      
-      end
-      sql = variables.inject([where_clause.join(' AND ')]){|sum, item|  sum << item}
-      @tools = @tools.where(sql)
-    end  
+    def arel_start
+       @tools ||= Tool.paginate default_pagination_params
+     end
+
+     def search_tools
+       where_clause = []
+       variables = []
+       params[:search].split.each do |t|
+         where_clause << "((UPPER(name) like UPPER(?)) OR (UPPER(description) like UPPER(?)))"
+         variables << "%#{t}%"
+         variables << "%#{t}%"      
+       end
+       sql = variables.inject([where_clause.join(' AND ')]){|sum, item|  sum << item}
+       begin
+         @tools = arel_start.search_by_name_or_description(params[:search], params[:search]).paginate default_pagination_params
+       rescue
+         @tools = arel_start.where(sql)
+         @tools = @tools.order('name asc')      
+       end
+     end
 end
