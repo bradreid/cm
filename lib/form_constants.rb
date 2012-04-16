9501b560828dd84278977149ce6120045b2af5c3
Brad Reid <bradreid20@gmail.com>
2012-04-15 23:11:52 -0500

    more translations

--- lib/form_constants.rb
+++ lib/form_constants.rb
@@ -1,26 +1,7 @@
 include ActionView::Helpers::TranslationHelper
 
 class FormConstants
-  def self.reading_levels
-    ['Grade 5/6', 'Grade 8', 'Grade 12', 'Bachelors', 'Masters', 'PhD']
-  end
-  
-  def self.jargon
-    ['0-10', '11-20', '21-30', '30+']
-  end
-  
-  def self.steps
-    ['1-3', '4-6', '7-10', '10+']
-  end  
-  
-  def self.focus
-    ["Social", "Economic", "Environment"]
-  end
-  
-  def self.time_units
-    ['Minute', 'Hour', 'Day', 'Week', 'Month', 'Year']
-  end  
-  
+
   def self.language
     [t(:en, :scope=>[:formconstants,:language]), t(:fr, :scope=>[:formconstants,:language]), t(:bi, :scope=>[:formconstants,:language]), t(:both, :scope=>[:formconstants,:language])]
   end  
