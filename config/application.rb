0e7c7ef43992002cab643c5bd69d501ab69adeae
Brad Reid <bradreid20@gmail.com>
2012-04-22 17:38:16 -0500

    fix case

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Cm
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W(#{config.root}/lib/ #{config.root}/lib/Reports/)
+    config.autoload_paths += %W(#{config.root}/lib/ #{config.root}/lib/reports/)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
