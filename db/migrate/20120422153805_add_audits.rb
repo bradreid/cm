87421d1b87798c4ed13911b700ff26d5f3d8a380
Brad Reid <bradreid20@gmail.com>
2012-04-22 10:50:29 -0500

    add migration

--- db/migrate/20120422153805_add_audits.rb
+++ db/migrate/20120422153805_add_audits.rb
@@ -1,7 +1,33 @@
 class AddAudits < ActiveRecord::Migration
   def self.up
+    create_table :server_request_logs, :force => true do |t|
+      t.integer :user_id
+      t.integer :session_id
+      t.integer :tic_id
+      t.integer :review_id
+      t.string :url
+      t.string :section
+      t.string :referrer
+      t.string :search
+      t.timestamps
+    end
+    
+    add_index :server_request_logs, :search
+    add_index :server_request_logs, :user_id
+    add_index :server_request_logs, :session_id
+    add_index :server_request_logs, :tic_id
+    add_index :server_request_logs, :review_id
+    add_index :server_request_logs, :section
+    
   end
 
   def self.down
+    remove_index :server_request_logs, :search
+    remove_index :server_request_logs, :section
+    remove_index :server_request_logs, :review_id
+    remove_index :server_request_logs, :tic_id
+    remove_index :server_request_logs, :session_id
+    remove_index :server_request_logs, :user_id
+    drop_table :server_request_logs
   end
 end
