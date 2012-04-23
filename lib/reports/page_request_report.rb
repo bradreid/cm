c78fb526f5676dca7d7436c5e6500d8b5097aeb9
Brad Reid <bradreid20@gmail.com>
2012-04-22 21:31:34 -0500

    mo better timezones

--- lib/reports/page_request_report.rb
+++ lib/reports/page_request_report.rb
@@ -7,10 +7,11 @@ class PageRequestReport < Report
       d.merge! today.to_s => [0,0]
       today = today.yesterday
     end
-    result = ActiveRecord::Base.connection.execute("select date(srl.created_at), count(*) from server_request_logs srl where srl.created_at between '#{@from.to_s(:db)}' and '#{@to.to_s(:db)}' group by date(srl.created_at) ")
+    offset = Time.now.formatted_offset.to_i
+    result = ActiveRecord::Base.connection.execute("select  date(srl.created_at AT TIME ZONE '#{offset.to_s}') AS date, count(*) from server_request_logs srl where srl.created_at between '#{@from.to_s(:db)}' and '#{@to.to_s(:db)}' group by date(srl.created_at AT TIME ZONE '#{offset.to_s}')")
     result.each{|row| d[row['date']][0] = row['count'].to_i if d[row['date']]}
     
-    result = ActiveRecord::Base.connection.execute("select date(srl.created_at), count(distinct(srl.session_id)) from server_request_logs srl where srl.created_at between '#{@from.to_s(:db)}' and '#{@to.to_s(:db)}' group by date(srl.created_at)")
+    result = ActiveRecord::Base.connection.execute("select date(srl.created_at AT TIME ZONE '#{offset.to_s}') AS date, count(distinct(srl.session_id)) from server_request_logs srl where srl.created_at between '#{@from.to_s(:db)}' and '#{@to.to_s(:db)}' group by date(srl.created_at AT TIME ZONE '#{offset.to_s}')")
     result.each{|row| d[row['date']][1] = row['count'].to_i if d[row['date']]}    
     d = d.sort
     return Date.parse(d.first.try(:first) || @from.to_s), d.map{|i| i[1][0]}, d.map{|i| i[1][1]}
