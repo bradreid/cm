0d9e75449529b66bccd4e3fa68d4519fb66873e6
Brad Reid <bradreid20@gmail.com>
2012-05-07 22:15:52 -0500

    some images

--- app/controllers/admin/stats/page_requests_controller.rb
+++ app/controllers/admin/stats/page_requests_controller.rb
@@ -1,9 +1,5 @@
 class Admin::Stats::PageRequestsController < AdminController
   def new
-<<<<<<< HEAD
- 
-=======
     @start_date, @data, @data2 = PageRequestReport.new(params).data
->>>>>>> 68345b738922fa5a6d0fe69b43914b7b91a8f4e1
   end
 end
