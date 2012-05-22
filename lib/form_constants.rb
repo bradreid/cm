28d2a8efd4fd282929dc3a7bf19fb5ccd1935031
Bowen Hui <bohuie@gmail.com>
2012-05-22 15:01:49 -0500

    added phases definitions and changed style

--- lib/form_constants.rb
+++ lib/form_constants.rb
@@ -13,6 +13,10 @@ class FormConstants
   def self.when
     [t(:organizing, :scope=>[:formconstants,:when]),t(:planning, :scope=>[:formconstants,:when]),t(:analyzing, :scope=>[:formconstants,:when]),t(:implementing, :scope=>[:formconstants,:when]),t(:reviewing, :scope=>[:formconstants,:when])]
   end
+
+  def self.whendefns
+    [t(:alt_org), t(:alt_plan), t(:alt_anal), t(:alt_impl), t(:alt_rev) ]
+  end
   
   def self.organizing_purpose
     [t(:org_partnership, :scope=>[:formconstants,:when]),t(:org_fundraising, :scope=>[:formconstants,:when]),t(:org_teambuilding, :scope=>[:formconstants,:when]),t(:org_engagement, :scope=>[:formconstants,:when])]
@@ -32,6 +36,26 @@ class FormConstants
   
   def self.analyzing_purpose
     [t(:anal_ass, :scope=>[:formconstants,:when]),t(:anal_comp, :scope=>[:formconstants,:when])]
+  end
+
+  def self.orgdefns
+    [ t(:alt_org_partnership), t(:alt_org_fundraising), t(:alt_org_teambuilding), t(:alt_org_engagement) ]
+  end
+
+  def self.plandefns
+    [ t(:alt_plan_vis), t(:alt_plan_act), t(:alt_plan_man) ]
+  end
+
+  def self.analdefns
+    [ t(:alt_anal_ass), t(:alt_anal_comp) ]
+  end
+
+  def self.impldefns
+    [ t(:alt_impl_natural), t(:alt_impl_cultural), t(:alt_impl_hr), t(:alt_impl_social), t(:alt_impl_gov), t(:alt_impl_financial), t(:alt_impl_comm) ]
+  end
+
+  def self.revdefns
+    [ t(:alt_rev_proj), t(:alt_rev_proc), t(:alt_rev_feedback) ]
   end
 
   def self.where
