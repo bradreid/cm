76ca870c430ed156303df63f1210a46df8ea43b5
Brad Reid <bradreid20@gmail.com>
2012-06-03 11:18:28 -0500

    update fb graph tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,13 @@ module ApplicationHelper
     end
     image_tag image
   end
+  
+  def image_url(source)
+    abs_path = image_path(source)
+    unless abs_path =~ /^http/
+      abs_path = "#{request.protocol}#{request.host_with_port}#{abs_path}"
+    end
+    abs_path
+  end
+    
 end
