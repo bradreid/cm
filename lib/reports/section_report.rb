507f80e5a44f7a6476c95e5a63943a9449c9ea95
Brad Reid <bradreid20@gmail.com>
2012-06-09 18:21:08 -0500

    update order

--- lib/reports/section_report.rb
+++ lib/reports/section_report.rb
@@ -2,7 +2,7 @@ class SectionReport < Report
   
   def data    
     result = ActiveRecord::Base.connection.execute("select  srl.section AS section, count(*) from server_request_logs srl where srl.section IS NOT NULL and srl.created_at between '#{@from.to_s(:db)}' and '#{@to.to_s(:db)}' group by srl.section")
-    result.to_a.map{|h| [h['section'], h['count']]}.sort{|x,y| y[1] <=> x[1]}
+    result.to_a.map{|h| [h['section'], h['count'].to_i]}.sort{|x,y| y[1] <=> x[1]}
   end
  
 end
