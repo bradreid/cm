ee652a9549a676e97a39d3419fa3f9a81e69bab1
Brad Reid <bradreid20@gmail.com>
2012-06-10 23:00:01 -0500

    fix rounding

--- lib/reports/section_report.rb
+++ lib/reports/section_report.rb
@@ -4,7 +4,7 @@ class SectionReport < Report
     result = ActiveRecord::Base.connection.execute("select  srl.section AS section, count(*) from server_request_logs srl where srl.section IS NOT NULL and srl.section <> 'admin' and srl.created_at between '#{@from.to_s(:db)}' and '#{@to.to_s(:db)}' group by srl.section")
     r = result.to_a.map{|h| [h['section'], h['count'].to_i]}.sort{|x,y| y[1] <=> x[1]}
     total = r.inject(0){|s, v| s += v[1]}
-    r.map{|i| [i[0],(i[1].to_f/total.to_f * 100).round(1)]}
+    r.map{|i| [i[0],(i[1].to_f/total.to_f * 100).round(2)]}
   end
  
 end
